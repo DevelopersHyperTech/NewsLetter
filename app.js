bae4de8f873c6868080db5cb43499f604932f0d1
DevelopersHyperTech <119522654+DevelopersHyperTech@users.noreply.github.com>
2023-01-12 16:56:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.post("/failure", function (req, res) {
   res.redirect("/")
 })
 
-app.listen(3000, 0.0.0.0, function () {
+app.listen(process.env.PORT || 3000 , function () {
   console.log("server is working");
 })
 // Api Key
